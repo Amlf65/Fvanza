1d3e479dadf0386ba84ad0db15e20a7f593790a1
Adolfo Lozano <amlf65@gmail.com>
2022-01-10 21:38:48 +0000

    Inicio Fvanza

---
+++ printPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div id="userInfo"></div>
+<!-- <h2>The window.print() Method</h2>
+
+<p>Click the button to print the current page.</p>
+
+<button onclick="window.print()">Print this page</button> -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+<script>
+$(document).ready(function(){
+    window.open("https://www.w3schools.com");
+});
+</script>
+
+
+</body>
+</html>
